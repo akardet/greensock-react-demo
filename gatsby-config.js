843a6cefbdf68710a9c6ae1efa6ec069c9ca436a
Sam Tancharoensuksavai <stancharoen@gmail.com>
2019-09-09 23:14:32 -0500

    add: greensock animations to circle

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,14 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `images`,
+    //     path: `${__dirname}/src/images,`,
+    //   },
+    // },
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,26 @@
-import React from "react"
+import React, { Component } from "react"
+import "../sass/main.scss"
+import { TimelineLite, Back } from "gsap/all"
+export default class index extends Component {
+  constructor(props) {
+    super(props)
+    this.myElement = null
+    this.myTween = null
+  }
 
-export default function index() {
-  return <div></div>
+  componentDidMount() {
+    this.myTween = new TimelineLite().to(this.myElement, 4, {
+      x: 400,
+      ease: Back.easeIn,
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Greensock and React</h1>
+        <div className="circle" ref={div => (this.myElement = div)} />;
+      </div>
+    )
+  }
 }
